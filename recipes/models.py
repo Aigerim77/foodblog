7f890f82668e7e114b7ee5ff885cd75c96e51034
Aigerim77 <77402879+Aigerim77@users.noreply.github.com>
2021-08-10 17:28:21 +0600

    added user fk in recipe model

--- recipes/models.py
+++ recipes/models.py
@@ -1,8 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Recipe(models.Model):
+    user = models.ForeignKey(
+        to=User, on_delete=models.SET_NULL, 
+        null=True, blank=True
+    )
     name = models.CharField(max_length=255)
-    location = models.CharField(max_length=255) #короткое описание
+    short_description = models.CharField(max_length=255) #короткое описание
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
